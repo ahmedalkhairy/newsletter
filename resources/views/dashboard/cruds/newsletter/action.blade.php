1847aad87ece7aa1ccb3b4a183bae740fc297167
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-05-03 04:09:10 +0300

    rahama and mayssa branches meraged to the master branch

--- resources/views/dashboard/cruds/newsletter/action.blade.php
+++ resources/views/dashboard/cruds/newsletter/action.blade.php
@@ -1,45 +1,21 @@
 <div class="d-inline-flex">
 
 
-<<<<<<< HEAD
-    <a class="btn btn-primary m-2" href="{{route('newsletters.show' , ['newsletter'=>$id])}}">Show</a>
+    <div class="m-2">
+        <a href="{{ route('newsletters.show', ['newsletter'=>$id]) }}"> <i class="fas fa-eye"> </i> </a>&nbsp;&nbsp;
+    </div>
 
-    <a class="btn btn-secondary m-2" href="{{route('newsletters.edit' , ['newsletter'=>$id])}}">Edit</a>
+    <div class="m-2">
+        <a href="{{ route('newsletters.edit', ['newsletter'=>$id]) }}"> <i class="fas fa-edit"></i> </a>&nbsp;&nbsp;
+    </div>
 
-    <form action="{{route('newsletters.changeStatus' , ['newsletter'=>$id])}}" method="POST">
-        @method('PATCH')
-        @csrf
-        <span class="kt-switch kt-switch--outline kt-switch--icon kt-switch--primary">
-            <label>
-                <input class="active-newsletter" type="checkbox" {{$active=="Active"?'checked="checked"':""}} name="active">
-                <span></span>
-            </label>
-        </span>
-    </form>
+    <div class="m-2">
+        <div class="custom-control custom-switch">
+
+            <input type="checkbox" class="custom-control-input" {{$active=="Active"?'checked="checked"':""}}
+                id="customSwitch{{$id}}">
+            <label class="custom-control-label" for="customSwitch{{$id}}"></label>
+        </div>
+    </div>
 
 </div>
-=======
-<a href="{{ route('newsletters.show', ['newsletter'=>$id]) }}">   <i class="fas fa-eye"> </i> </a>&nbsp&nbsp
-            <a href="{{ route('newsletters.edit', ['newsletter'=>$id]) }}"> <i class="fas fa-edit"></i> </a>&nbsp&nbsp
-
-
-
-                  <form action="{{route('newsletters.changeStatus' , ['newsletter'=>$id])}}" method="POST">
-        @method('PATCH')
-        @csrf
-        
-
-
-        <div class="form-group">
-                    <div class="custom-control custom-switch">
-                    
-                      <input type="checkbox" class="custom-control-input" {{$active=="Active"?'checked="checked"':""}} id="customSwitch1">
-                      <label class="custom-control-label" for="customSwitch1"></label>
-                    </div>
-                  </div>
-        
-    </form>
-
-   </div>
- 
->>>>>>> 645f349a11e98d56a267478f393eda1049a505d9
